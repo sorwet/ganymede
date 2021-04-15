acf66b31446832f0c367d87d8014ca8e7cacfa9c
nartesfasrum <nart@tirefire.pw>
2021-04-15 18:47:40 -0500

    initial dotpost acknowledgement functionality

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ganymede
-cupbearer to ~zod
+
+Cupbearer to ~zod. A bot for acknowledging dotposts.
+
+Note that this software is currently under development, and I am not responsible for any loss of confidentiality, availability, or integrity of data resulting from use of this software.
+
+## Requirements
+
+* [Quinnat](https://github.com/midsum-salrux/quinnat)
+
+## Setup
+
+Install Quinnat via `pip`:
+
+Copy `example.ini` to `default.ini`, and modify it as appropriate to meet your needs.
+
+Add your bot to the chats you want it to acknowledge posts in.
+
+Start the bot:
+
+`python3 main.py`
---
+++ main.py
@@ -0,0 +1,24 @@
+import configparser, quinnat
+
+config = configparser.ConfigParser()
+config.read('default.ini')
+
+urbitUrl = config['URBIT']['urbitUrl']
+urbitId = config['URBIT']['urbitId']
+urbitCode = config['URBIT']['urbitCode']
+urbitChat = config['URBIT']['urbitChat']
+urbitHost = config['URBIT']['urbitHost']
+urbitClient = quinnat.Quinnat(urbitUrl, urbitId, urbitCode)
+
+messageWelcome = config['MESSAGES']['messageWelcome']
+messageAck = config['MESSAGES']['messageAck']
+
+urbitClient.connect()
+
+urbitClient.post_message(urbitHost, urbitChat, {"text": messageWelcome})
+
+def dot_ack(message, replier):
+    if "." == message.full_text:
+        replier({"text": messageAck})
+
+urbitClient.listen(dot_ack)
